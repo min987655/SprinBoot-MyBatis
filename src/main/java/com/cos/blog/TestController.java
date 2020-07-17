db70a01edccc5940fa9391ab693673b8b8c2a607
1com <123@nate.com>
2020-07-17 10:34:21 +0900

    SpringBoot-기본셋팅

---
+++ src/main/java/com/cos/blog/TestController.java
@@ -0,0 +1,14 @@
+package com.cos.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+	@GetMapping({"","/"})
+	public String index() {
+		return "index";
+	}
+	
+}
